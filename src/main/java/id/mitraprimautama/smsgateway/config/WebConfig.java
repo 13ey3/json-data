c546b70be5bdc03aeea49d5d21dfe9538f47aa7c
13ey3 <saepulloh.ramdani2@gmail.com>
2020-04-17 11:17:05 +0700

    buat test bro

--- src/main/java/id/mitraprimautama/smsgateway/config/WebConfig.java
+++ src/main/java/id/mitraprimautama/smsgateway/config/WebConfig.java
@@ -2,6 +2,7 @@ package id.mitraprimautama.smsgateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,7 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
       registry.addViewController("/login").setViewName("login");
-  }
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      registry.addResourceHandler("/webjars/**")
+              .addResourceLocations("/webjars/");
+    }
 
   @Bean
   public LayoutDialect layoutDialect() {
